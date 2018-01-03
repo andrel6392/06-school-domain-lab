0fe1dfb46112cb9cc83242b31eba40e8c767332c
andrel6392 <andrel6392@hstat.org>
2018-01-03 17:31:56 +0000

    finish lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+
+class School
+    attr_accessor :roster
+    
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    
+    def add_student(name, grade)
+        if @roster[grade].nil?
+            @roster[grade] = [name]
+        else
+            @roster[grade] << name
+        end
+    end
+    
+    def grade(num)
+        @roster[num]
+    end
+    
+    def sort
+        roster_sorted = {}
+        @roster.map do |grade, students|
+            roster_sorted[grade] = students.sort
+        end
+        roster_sorted
+    end
+end
